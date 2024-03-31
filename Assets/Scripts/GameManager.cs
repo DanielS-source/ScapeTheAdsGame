f1e21756128acdf9207afa24641dafe354d7ce1e
DanielS-source <daniel.silva.iglesias@udc.es>
2024-03-31 11:28:01 +0200

    Cleaned code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.IO;
 using System.Net.Sockets;
-using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using TMPro;
-using UnityEditor.PackageManager;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
