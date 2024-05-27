cb95bd4863a6c804138da11c61603258452f4a0a
juanramil <juan.ramil@udc.es>
2024-05-27 22:01:35 +0200

    Some fixes in compass, adding correctly the scenes, and finishing correctly the infinite mode

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -2,26 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimerScript : MonoBehaviour
 {
+    public TimerScript instance;
+
     public float startTimer = 20;
 
     private float timer = 0;
 
     public TextMeshProUGUI timerText;
     public TextMeshProUGUI debugText;
+    private void Awake()
+    {
+
+        //Singleton and adding this gameObject to DontDestroyOnLoad
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        bool finished = false;
         timer += Time.deltaTime;
         
         timerText.text = "" + (startTimer - timer).ToString("f2");
         debugText.text = GameHandler.instance.debugText;
 
-        if ((startTimer - timer) < 0)
+        if (!finished && (startTimer - timer) < 0)
         {
+            finished = true;
             GameHandler.instance.TimeOver();
         }
     }
